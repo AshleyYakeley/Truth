510c97fe2f283372e257b1e59732fd039eb8b0e3
Ashley Yakeley <ashley@semantic.org>
2009-01-10 21:56:29 -0800

    traversableCodec
    darcs-hash:bad47ab31f15b2e9765d6e162ad5621410748fd1

--- Changes/Data/Codec.hs
+++ Changes/Data/Codec.hs
@@ -1,6 +1,7 @@
 module Data.Codec where
 {
 	import Control.Category;
+	import Data.Traversable;
 	import Prelude hiding (id,(.));
 
 	data Codec a b = MkCodec
@@ -14,5 +15,12 @@ module Data.Codec where
 	{
 		id = MkCodec Just id;
 		(MkCodec bmc cb) . (MkCodec amb ba) = MkCodec (\a -> (amb a) >>= bmc) (ba . cb);
+	};
+	
+	traversableCodec :: (Traversable f) => Codec a b -> Codec (f a) (f b);
+	traversableCodec codec = MkCodec
+	{
+		decode = traverse (decode codec),
+		encode = fmap (encode codec)
 	};
 }
