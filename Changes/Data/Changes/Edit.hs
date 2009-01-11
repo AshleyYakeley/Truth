610d695647d8bb9f4f3eaf214d6b26e58ce87b58
Ashley Yakeley <ashley@semantic.org>
2009-01-11 03:04:02 -0800

    functorOneLens
    darcs-hash:f3277c57fb49856c602b5156bcc1e5674d9b4b8c

--- Changes/Data/Changes/Edit.hs
+++ Changes/Data/Changes/Edit.hs
@@ -6,24 +6,53 @@ module Data.Changes.Edit where
 	import Control.Category;
 	import Prelude hiding (id,(.));
 
+	class (Functor f) => FunctorOne f where
+	{
+		retrieveOne :: f a -> Either (f b) a;
+	};
+	-- retrieveOne (fmap f w) = fmap f (retrieveOne w)
+	-- case (retrieveOne w) of {Left w' -> fmap f w';Right a -> fmap (\_ -> a) w;} = w
+
+	instance FunctorOne Maybe where
+	{
+		retrieveOne (Just a) = Right a;
+		retrieveOne Nothing = Left Nothing;
+	};
+	
+	instance FunctorOne (Either a) where
+	{
+		retrieveOne (Right b) = Right b;
+		retrieveOne (Left a) = Left (Left a);
+	};
+
 	data Edit a where
 	{
 		ReplaceEdit :: a -> Edit a;
 		StateLensEdit :: (Eq state) => StateLens state a b -> state -> Edit b -> Edit a;
+		FunctorOneEdit :: (FunctorOne f) => Edit b -> Edit (f b);
 	};
 	
-	applyAndInvertEdit :: a -> Edit a -> (a,Edit a);
-	applyAndInvertEdit olda (ReplaceEdit newa) = (newa,ReplaceEdit olda);
+	applyAndInvertEdit :: a -> Edit a -> (a,Maybe (Edit a));
+	applyAndInvertEdit olda (ReplaceEdit newa) = (newa,Just (ReplaceEdit olda));
 	applyAndInvertEdit olda (StateLensEdit lens oldstate editb) = result where
 	{
 		oldb = slensGet lens oldstate olda;
 		(newb,invb) = applyAndInvertEdit oldb editb;
 		result = case slensPutback lens oldstate newb olda of
 		{
-			Just (newa,newstate) -> (newa,StateLensEdit lens newstate invb);
+			Just (newa,newstate) -> (newa,fmap (StateLensEdit lens newstate) invb);
 			_ -> error "bad lens edit";
 		};
 	};
+	applyAndInvertEdit oldfb (FunctorOneEdit editb) = (newfb,mneweditfb) where
+	{
+		newfb = fmap (applyEdit editb) oldfb;
+		mneweditfb = case (retrieveOne oldfb) of
+		{
+			Left _ -> Nothing;
+			Right oldb -> fmap FunctorOneEdit (invertEdit oldb editb);
+		};
+	};	
 	
 	applyEdit :: Edit a -> a -> a;
 	applyEdit edit olda = fst (applyAndInvertEdit olda edit);
@@ -32,7 +61,7 @@ module Data.Changes.Edit where
 	applyEdits [] = id;
 	applyEdits (e:es) = (applyEdits es) . (applyEdit e);
 	
-	invertEdit :: a -> Edit a -> Edit a;
+	invertEdit :: a -> Edit a -> Maybe (Edit a);
 	invertEdit olda edit = snd (applyAndInvertEdit olda edit);
 	
 	commutableEdits :: (Eq a) => Edit a -> Edit a -> a -> Maybe a;
@@ -50,7 +79,43 @@ module Data.Changes.Edit where
 		slensGet :: state -> a -> b,
 		slensPutback :: state -> b -> a -> Maybe (a,state)
 	};
-	
+
+	functorOneLens :: forall f state a b. (FunctorOne f) => StateLens state a b -> StateLens state (f a) (f b);
+	functorOneLens lens = MkStateLens
+	{
+		slensWitness = undefined,
+		slensStateWitness = undefined,
+		slensUpdate = update,
+		slensGet = get,
+		slensPutback = putback
+	}
+	where
+	{
+		update :: f a -> Edit (f a) -> state -> (state,Maybe (Edit (f b)));
+		update fa (FunctorOneEdit ea) state | Right a <- retrieveOne fa = let
+		{
+			(state',meb) = slensUpdate lens a ea state;
+		} in (state',fmap FunctorOneEdit meb);
+		update fa efa state = (state,Just (ReplaceEdit (get state (applyEdit efa fa))));
+		
+		get state = fmap (slensGet lens state);
+
+		putback state fb fa = case retrieveOne fb of
+		{
+			-- Left fa' -> Just (fa',state);
+			Left _ -> Nothing;		-- pushing nothing on something fails
+			Right b -> case retrieveOne fa of
+			{
+				Left _ -> Nothing;	-- pushing something on nothing fails
+				Right a -> do
+				{
+					(a',state') <- slensPutback lens state b a;
+					return (fmap (\_ -> a') fa,state');
+				};
+			};
+		};
+	};
+
 	data SimpleLens a b = MkSimpleLens
 	{
 		simpleLensWitness :: TFWitness IOWitness a b,
