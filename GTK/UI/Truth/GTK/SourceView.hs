4eeb7d96d9958ed9c0323b6a4a87ba16fbd5bb1d
Ashley Yakeley <ashley@semantic.org>
2009-02-22 23:31:38 -0800

    checkButtonViewFactory, attempt at a maybeViewFactory
    darcs-hash:7bccf0d9ea806d60ec37d719aaa0499a7a966d4c

--- GTK/UI/Truth/GTK/SourceView.hs
+++ GTK/UI/Truth/GTK/SourceView.hs
@@ -13,6 +13,67 @@ module UI.Truth.GTK.SourceView where
 	--type ViewFactory context a = Object context a -> (Selection -> IO ()) -> IO View;
 	type ViewFactory context a = Object context a -> IO View;
 
+	checkButtonViewFactory :: String -> ViewFactory context Bool;
+	checkButtonViewFactory name obj = do
+	{
+		(widget,sub) <- objSubscribe obj (\s -> do
+		{
+			widget <- checkButtonNew;
+			set widget [buttonLabel := name,toggleButtonActive := s];
+			return widget;
+		})
+		(\widget edit -> do
+		{
+			s <- get widget toggleButtonActive;
+			set widget [toggleButtonActive := (applyEdit edit s)];
+		});
+		onClicked widget (do
+		{
+			_ <- subPush sub (do
+			{
+				s <- get widget toggleButtonActive;
+				return (ReplaceEdit s);
+			});
+			return ();
+		});
+		return (MkView
+		{
+			viewWidget = widget,
+			viewRequestClose = do
+			{
+				subClose sub;
+				return True;
+			}
+		});
+	};
+{-
+	maybeViewFactory :: a -> ViewFactory context a -> ViewFactory context (Maybe a);
+	maybeViewFactory emptyval factory objma = do
+	{
+		(widget,sub) <- objSubscribe objma (\a -> do
+		{
+			frame <- frameNew;
+			createButton <- buttonNew;
+			case a of
+			{
+				Just _ -> set frame [containerChild := w]
+		})
+		(\widget edit -> do
+		{
+		})
+		;
+		onClicked createButton (subPush (return (ReplaceEdit emptyval)));
+		return (MkView
+		{
+			viewWidget = widget,
+			viewRequestClose = do
+			{
+				subClose sub;
+				return True;
+			}
+		});
+	};
+-}
 	sourceViewBrowser :: ViewFactory context a;
 	sourceViewBrowser _ = do
 	{
