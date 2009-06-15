d0f73ffebd55b12ffc92ebf3b544d5d9d8300a3e
Ashley Yakeley <ashley@semantic.org>
2009-06-15 01:02:17 -0700

    add start of Cabal project
    Ignore-this: 505e99f6b3dac23284622ebe1b0f90bd
    
    darcs-hash:b7201db4c953af60fa1751d5f7ea5f26e65c75d7

---
+++ Cabal/Data/Changes/Cabal.hs
@@ -0,0 +1,49 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Data.Changes.Cabal where
+{
+	import Distribution.PackageDescription.Parse;
+	import Distribution.PackageDescription;
+	import Distribution.Package;
+	import Distribution.ParseUtils;
+	import Data.Changes;
+	import Data.Result;
+
+	interpretPackageDescription :: WholeLens String (Result PError GenericPackageDescription);
+	interpretPackageDescription = resultWholeLens (parseResultToResult . parsePackageDescription) show where
+	{
+		parseResultToResult :: ParseResult a -> Result PError a;
+		parseResultToResult (ParseFailed perr) = FailureResult perr;
+		parseResultToResult (ParseOk _ a) = SuccessResult a;
+	};
+
+	instance IsTuple GenericPackageDescription where
+	{
+		type TList GenericPackageDescription = 
+			(PackageDescription,
+			([Flag],
+			(Maybe (CondTree ConfVar [Dependency] Library),
+			([(String, CondTree ConfVar [Dependency] Executable)],
+			()))));
+		fromListTuple (a,(b,(c,(d,())))) = GenericPackageDescription a b c d;
+		toListTuple (GenericPackageDescription a b c d) = (a,(b,(c,(d,()))));
+	};
+
+	instance Editable GenericPackageDescription where
+	{
+		type PartEdit GenericPackageDescription = TListPartEdit (TList GenericPackageDescription);
+	};
+{-
+
+GenericPackageDescription	
+packageDescription :: PackageDescription	
+genPackageFlags :: [Flag]	
+condLibrary :: Maybe (CondTree ConfVar [Dependency] Library)
+condExecutables :: 	
+
+
+	instance (Editable a,Editable b) => Editable (a,b) where
+	{
+		type PartEdit (a,b) = TListPartEdit (a,(b,()));
+	};
+-}
+}
--- Makefile
+++ Makefile
@@ -2,8 +2,10 @@ build:
 	cd Changes && make test
 	cd Linux && make test
 	cd GTK && make install
+	cd Cabal && make install
 
 clean:
 	cd Changes && make clean
 	cd Linux && make clean
 	cd GTK && make clean
+	cd Cabal && make clean
