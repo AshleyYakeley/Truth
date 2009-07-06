91182145e8d5b285551aa061776a25616693c97c
Ashley Yakeley <ashley@semantic.org>
2009-07-06 02:16:20 -0700

    mapView
    Ignore-this: 7a00aa619defd39169147be6b14e10e3
    
    darcs-hash:20990b0f6a3f9d53443a1cfe4b81f5b32404f58b

--- Changes/Data/Changes/View.hs
+++ Changes/Data/Changes/View.hs
@@ -8,8 +8,14 @@ module Data.Changes.View where
 		vrWidget :: w,
 		vrUpdate :: Edit a -> IO ()
 	};
+	
+	mapViewResult :: (w1 -> w2) -> ViewResult w1 a -> ViewResult w2 a;
+	mapViewResult f (MkViewResult w1 u) = MkViewResult (f w1) u;
 
 	type View w a = a -> Push a -> IO (ViewResult w a);
+	
+	mapView :: (w1 -> w2) -> View w1 a -> View w2 a;
+	mapView f view a push = fmap (mapViewResult f) (view a push);
 
 	subscribeView :: View w a -> Subscribe a -> IO (Subscribe a,w,IO ());
 	subscribeView view subscribe = do
