56cf39396a2a83bf39906f73ad4992582acd541f
Ashley Yakeley <ashley@semantic.org>
2010-09-24 01:11:13 -0700

    beginnings of Merge
    Ignore-this: 87fccb9e2a7e0022654cea79a55fa917
    
    darcs-hash:7aad26c08ba2df859bf97069e38c4eefabc4d5b6

---
+++ Core/Truth/Edit/Merge.hs
@@ -0,0 +1,27 @@
+module Truth.Edit.Merge where
+{
+    import Truth.Edit.Edit;
+--    import Truth.Edit.Import;
+
+    data EditStep edit = MkEditStep edit [edit];
+
+    data Merge edit = MkMerge (Subject edit) [EditStep edit];
+
+    data MergeEdit edit = AddOverEdit edit | RemoveOverEdit | UnderEdit edit;
+{-
+    instance (Edit edit) => Edit (MergeEdit edit) where
+    {
+        type Subject (MergeEdit edit) = Merge edit;
+
+        applyEdit (AddOverEdit edit)
+    };
+
+    mergeSubjectLens :: EditLens' Identity (MergeEdit edit) edit;
+    mergeSubjectLens = MkEditLens
+    {
+        editLensSimple :: Lens' m (Subject edita) (Subject editb),
+        editLensUpdate :: edita -> ConstFunction (Subject edita) (Maybe editb),
+        editLensPutEdit :: editb -> ConstFunction (Subject edita) (m edita)
+    };
+-}
+}
