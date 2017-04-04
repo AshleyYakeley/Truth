91c9fe32fc7a7b4d12a6c3cec48f080cfdd858b2
Ashley Yakeley <ashley@yakeley.org>
2017-04-03 21:20:54 -0700

    reity: Template: improved handling of types

--- reity/src/Data/Reity/Template.hs
+++ reity/src/Data/Reity/Template.hs
@@ -21,13 +21,33 @@ module Data.Reity.Template(declInfo) where
         write stmt;
     };
 
+    data SimpleType = VarType Name | AppType SimpleType SimpleType | ConsType Type | FamilyType Name;
+
+    typeToSimple :: Type -> M SimpleType;
+    typeToSimple (ParensT tp) = typeToSimple tp;
+    typeToSimple (SigT tp _) = typeToSimple tp;
+    typeToSimple (VarT name) = return $ VarType name;
+    typeToSimple (AppT t1 t2) = AppType <$> typeToSimple t1 <*> typeToSimple t2;
+    typeToSimple (InfixT t1 name t2) = (\st1 st2 -> AppType (AppType (ConsType $ ConT name) st1) st2) <$> typeToSimple t1 <*> typeToSimple t2;
+    typeToSimple (ForallT _ _ _) = lift $ declFail "forall";
+    typeToSimple WildCardT = lift $ declFail "_ in type";
+    typeToSimple tp@(ConT name) = do
+    {
+        ninfo <- lift $ reify name;
+        case ninfo of
+        {
+            FamilyI _ _ -> return $ FamilyType name;
+            _ -> return $ ConsType tp;
+        };
+    };
+    typeToSimple tp = return $ ConsType tp;
+
     declFail :: String -> Q a;
     declFail s = fail $ "declInfo: " ++ s;
 
-    deconstruct :: Name -> Type -> M [(Name,Name)];
-    deconstruct subjectN (ParensT tp) = deconstruct subjectN tp;
-    deconstruct subjectN (VarT typeN) = return [(typeN,subjectN)];
-    deconstruct subjectN (AppT tp1 tp2) = do
+    deconstruct :: Name -> SimpleType -> M [(Name,Name)];
+    deconstruct subjectN (VarType typeN) = return [(typeN,subjectN)];
+    deconstruct subjectN (AppType tp1 tp2) = do
     {
         sN1 <- lift $ newName "_f";
         sN2 <- lift $ newName "_a";
@@ -36,40 +56,41 @@ module Data.Reity.Template(declInfo) where
         vars2 <- deconstruct sN2 tp2;
         return $ vars1 ++ vars2;
     };
-    -- more cases here
-    deconstruct subjectN tp = do
+    deconstruct subjectN (ConsType tp) = do
     {
         writeQ $ bindS [p|ReflH|] [e|testHetEquality (info :: R.Info $(return $ tp)) $(varE subjectN)|];
         return [];
     };
+    deconstruct _ (FamilyType name) = lift $ declFail $ "type family not allowed in instance: " ++ show name;
 
-    constructInfoExpr :: Name -> [(Name,Name)] -> Type -> M Exp;
-    constructInfoExpr knowledgeN varMap (ParensT tp) = constructInfoExpr knowledgeN varMap tp;
-    constructInfoExpr knowledgeN varMap (AppT tp1 tp2) = do
+    constructInfoExpr :: Name -> [(Name,Name)] -> SimpleType -> M Exp;
+    constructInfoExpr knowledgeN varMap (AppType tp1 tp2) = do
     {
         exp1 <- constructInfoExpr knowledgeN varMap tp1;
         exp2 <- constructInfoExpr knowledgeN varMap tp2;
         lift $ [e|R.applyInfo $(return exp1) $(return exp2)|];
     };
-    constructInfoExpr _knowledgeN varMap (VarT typeN) = case lookup typeN varMap of
+    constructInfoExpr _knowledgeN varMap (VarType typeN) = case lookup typeN varMap of
     {
         Just subjectN -> return $ VarE subjectN;
         Nothing -> lift $ declFail $ "type variable not found: " ++ show typeN;
     };
-    -- more cases here
-    constructInfoExpr _knowledgeN _varMap tp = lift [e|info :: R.Info $(return $ tp)|];
+    constructInfoExpr _knowledgeN _varMap (ConsType tp) = lift [e|info :: R.Info $(return $ tp)|];
+    constructInfoExpr _knowledgeN _varMap (FamilyType name) = lift $ declFail $ "NYI: type family not yet allowed in context: " ++ show name;
 
     matchContext :: Name -> [(Name,Name)] -> Type -> M ();
-    matchContext knowledgeN varMap consT = do
+    matchContext knowledgeN varMap tp = do
     {
-        infoExpr <- constructInfoExpr knowledgeN varMap consT;
+        st <- typeToSimple tp;
+        infoExpr <- constructInfoExpr knowledgeN varMap st;
         writeQ $ bindS [p|R.ConstraintFact|] [e|ask $(varE knowledgeN) $(return infoExpr)|];
     };
 
     instanceStmts :: Name -> Name -> [Type] -> Type -> M ();
     instanceStmts knowledgeN subjectN ctxt tp = do
     {
-        varMap <- deconstruct subjectN tp;
+        st <- typeToSimple tp;
+        varMap <- deconstruct subjectN st;
         traverse_ (matchContext knowledgeN varMap) ctxt;
         matchStmtE <- lift [e|return R.ConstraintFact|];
         write $ NoBindS matchStmtE;
