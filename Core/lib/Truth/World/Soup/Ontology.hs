f1099e3dbd449a3c755be98568823c4fe2573960
Ashley Yakeley <ashley@yakeley.org>
2017-08-27 01:04:06 -0700

    Soup.Ontology: beginnings

---
+++ Core/lib/Truth/World/Soup/Ontology.hs
@@ -0,0 +1,80 @@
+module Truth.World.Soup.Ontology where
+{
+    import Truth.Core.Import;
+    import Truth.Core;
+    import Data.UUID;
+    import Truth.World.Soup.Schema;
+
+
+    uuid :: String -> UUID;
+    uuid s = fromMaybe (error $ "couldn't parse UUID " ++ show s) $ Data.UUID.fromString s;
+
+    data ViewSoupSimpleProperty = MkViewSoupSimpleProperty
+    {
+        spropName :: String,
+        spropMorphism :: SoupMorphism (WholeEdit (Maybe UUID)) (WholeEdit (Maybe UUID)),
+        spropType :: ViewSoupType
+    };
+
+    data ViewSoupListProperty = MkViewSoupListProperty
+    {
+        lpropName :: String,
+        lpropMorphism :: SoupMorphism (WholeEdit (Maybe UUID)) (FiniteSetEdit UUID),
+        lpropType :: ViewSoupType
+    };
+
+    data ViewSoupProperty = SimpleViewSoupProperty ViewSoupSimpleProperty | ListViewSoupProperty ViewSoupListProperty;
+
+    data ViewSoupPrimitive = forall edit. MkViewSoupPrimitive
+    {
+        primType :: TypeInfo edit
+    };
+
+    data ViewSoupType = EntityViewSoupType [ViewSoupProperty] | PrimitiveViewSoupType ViewSoupPrimitive;
+
+    data ViewSoupValue = MkViewSoupValue UUID ViewSoupType;
+
+
+    -- example ontology
+
+    personType :: ViewSoupType;
+    personType = EntityViewSoupType [personName,personMother,personFather];
+
+    personName :: ViewSoupProperty;
+    personName = let
+    {
+        spropName = "name";
+        spropMorphism = predicateSoupMorphism $ uuid "498260df-6a8a-44f0-b285-68a63565a33b";
+        spropType = PrimitiveViewSoupType $ MkViewSoupPrimitive $ typeInfo @(StringEdit Text);
+    } in SimpleViewSoupProperty MkViewSoupSimpleProperty{..};
+
+    personMother :: ViewSoupProperty;
+    personMother = let
+    {
+        spropName = "mother";
+        spropMorphism = predicateSoupMorphism $ uuid "3afce58f-b7eb-4b11-8a75-2d66afd4d085";
+        spropType = personType;
+    } in SimpleViewSoupProperty MkViewSoupSimpleProperty{..};
+
+    personFather :: ViewSoupProperty;
+    personFather = let
+    {
+        spropName = "father";
+        spropMorphism = predicateSoupMorphism $ uuid "c005705f-9259-4d24-9713-db28a6e4f7d5";
+        spropType = personType;
+    } in SimpleViewSoupProperty MkViewSoupSimpleProperty{..};
+
+    peopleCollection :: ViewSoupProperty;
+    peopleCollection = let
+    {
+        lpropName = "people";
+        lpropMorphism = predicateInverseSoupMorphism $ uuid "f06efa5e-190f-4e5d-8633-495c5683c124";
+        lpropType = personType;
+    } in ListViewSoupProperty MkViewSoupListProperty{..};
+
+    rootType :: ViewSoupType;
+    rootType = EntityViewSoupType [peopleCollection];
+
+    rootValue :: ViewSoupValue;
+    rootValue = MkViewSoupValue (uuid "78baed51-cb05-46b5-bcb4-49031532b890") rootType;
+}
