5e04ab746a5bf12034b7ee7544a65186a0f8c4eb
Ashley Yakeley <ashley@semantic.org>
2017-11-03 05:29:06 -0700

    Pinafore: remove unused modules; clean up test.pui

--- pinafore/lib/Pinafore.hs
+++ pinafore/lib/Pinafore.hs
@@ -3,17 +3,13 @@ module Pinafore
     module Pinafore.AsText,
     module Pinafore.Edit,
     module Pinafore.Query,
-    module Pinafore.Ontology,
     module Pinafore.SQLite,
-    module Pinafore.Example,
     module Pinafore.Window,
 ) where
 {
     import Pinafore.AsText;
     import Pinafore.Edit;
     import Pinafore.Query;
-    import Pinafore.Ontology;
     import Pinafore.SQLite;
-    import Pinafore.Example;
     import Pinafore.Window;
 }
