cea784120c33b721f8451e59cc9e10cf0c6d3571
Ashley Yakeley <ashley@semantic.org>
2018-04-05 19:25:09 -0700

    pinafore: default data directory in user data dir

--- pinafore/app/Main.hs
+++ pinafore/app/Main.hs
@@ -5,17 +5,19 @@ module Main
 import qualified Options.Applicative as O
 import Pinafore
 import Shapes
+import System.Directory
+import System.Environment.XDG.BaseDir
 import Truth.UI.GTK
 
 data Options
     = ExprDocOption
-    | RunOption FilePath
+    | RunOption (Maybe FilePath)
                 [FilePath]
 
 optParser :: O.Parser Options
 optParser =
     (O.flag' ExprDocOption $ O.long "doc") <|>
-    RunOption <$> (O.strOption (O.long "db")) <*> (O.many $ O.strArgument mempty)
+    RunOption <$> (O.optional $ O.strOption (O.long "data")) <*> (O.many $ O.strArgument mempty)
 
 main :: IO ()
 main =
@@ -25,14 +27,20 @@ main =
             ExprDocOption -> do
                 for_ (predefinedDoc @PinaforeEdit) $ \(name, desc) -> putStrLn $ (show name) ++ " :: " ++ unpack desc
                 putStrLn $ "<file> :: " ++ unpack filePinaforeType
-            RunOption dirpath fpaths -> let
-                doFile :: FilePath -> Text -> IO ()
-                doFile fpath text = sqlitePinaforeMain dirpath (fpath, text) createWindow
-                in case fpaths of
-                       [] -> do
-                           ptext <- getContents
-                           doFile "<stdin>" $ decodeUtf8 $ toStrict ptext
-                       _ ->
-                           for_ fpaths $ \fpath -> do
-                               ptext <- readFile fpath
-                               doFile fpath $ decodeUtf8 $ toStrict ptext
+            RunOption mdirpath fpaths -> do
+                dirpath <-
+                    case mdirpath of
+                        Just dirpath -> return dirpath
+                        Nothing -> getUserDataDir "pinafore"
+                createDirectoryIfMissing True dirpath
+                let
+                    doFile :: FilePath -> Text -> IO ()
+                    doFile fpath text = sqlitePinaforeMain dirpath (fpath, text) createWindow
+                case fpaths of
+                    [] -> do
+                        ptext <- getContents
+                        doFile "<stdin>" $ decodeUtf8 $ toStrict ptext
+                    _ ->
+                        for_ fpaths $ \fpath -> do
+                            ptext <- readFile fpath
+                            doFile fpath $ decodeUtf8 $ toStrict ptext
