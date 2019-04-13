92c968bf346947c8db0b9f1e80e49a6131d4bd08
Ashley Yakeley <ashley@semantic.org>
2019-04-12 22:29:13 -0700

    debug: move debugging code to thread-trace package

--- truth-core/lib/Truth/Debug.hs
+++ truth-core/lib/Truth/Debug.hs
@@ -1,51 +1,7 @@
-module Truth.Debug where
+module Truth.Debug(module Debug.ThreadTrace,module Truth.Debug) where
 
-import Data.Time.Clock.System
-import Debug.Trace
-import System.IO.Unsafe
 import Truth.Core.Import
-
-contextStr :: String -> String -> String
-contextStr "" b = b
-contextStr a b = a ++ ": " ++ b
-
-traceIOM :: MonadIO m => String -> m ()
-traceIOM msg =
-    liftIO $ do
-        MkSystemTime s ns <- getSystemTime
-        th <- myThreadId
-        let
-            showMod :: Int -> Word32 -> String
-            showMod 0 _ = ""
-            showMod n x = showMod (pred n) (div x 10) <> show (mod x 10)
-        traceIO $ show s <> "." <> showMod 9 ns <> ": " <> show th <> ": " <> msg
-
-traceBracketArgs :: MonadIO m => String -> String -> (r -> String) -> m r -> m r
-traceBracketArgs s args showr ma = do
-    traceIOM $
-        s ++
-        " [" ++
-        (if null args
-             then ""
-             else " " ++ args)
-    a <- ma
-    let ret = showr a
-    traceIOM $
-        s ++
-        " ]" ++
-        (if null ret
-             then ""
-             else " => " ++ ret)
-    return a
-
-traceBracket :: MonadIO m => String -> m r -> m r
-traceBracket s = traceBracketArgs s "" (\_ -> "")
-
-tracePure :: String -> a -> a
-tracePure s = seq (unsafePerformIO (traceIOM s))
-
-tracePureBracket :: Monad m => String -> m a -> m a
-tracePureBracket s ma = (tracePure (s ++ " [") ma) >>= (\a -> return $ tracePure (s ++ " ]") a)
+import Debug.ThreadTrace
 
 class TraceThing t where
     traceThing :: String -> t -> t
