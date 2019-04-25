514fa69645cecd860690d854782e752a093bcb33
Ashley Yakeley <ashley@semantic.org>
2019-04-25 10:24:03 -0700

    shapes: Clock: use MVar instead of mask

--- shapes/src/Control/Clock.hs
+++ shapes/src/Control/Clock.hs
@@ -17,25 +17,29 @@ clock ndtInterval call = do
     let
         musInterval :: Integer
         musInterval = truncate $ nominalDiffTimeToSeconds ndtInterval * 1000000
+    var <- liftIO newEmptyMVar
     thread <-
         liftIO $
-        uninterruptibleMask_ $
-        forkIOWithUnmask $ \unmask ->
-            handle (\MkCancelled -> return ()) $
-            forever $ do
-                MkSystemTime s ns <- getSystemTime
-                let
-                    musTime :: Integer
-                    musTime = (toInteger s) * 1000000 + div (toInteger ns) 1000
-                    musRemaining :: Integer
-                    musRemaining = musInterval - (mod musTime musInterval)
-                    musNext :: Integer
-                    musNext = musTime + musRemaining
-                    sysTimeNext :: SystemTime
-                    sysTimeNext =
-                        MkSystemTime (fromInteger $ div musNext 1000000) $ fromInteger $ (mod musNext 1000000) * 1000
-                    utcTimeNext :: UTCTime
-                    utcTimeNext = systemToUTCTime sysTimeNext
-                unmask $ threadDelay $ fromInteger musRemaining
-                call utcTimeNext
-    lifeCycleClose $ throwTo thread MkCancelled
+        forkIO $
+        handle (\MkCancelled -> return ()) $
+        forever $ do
+            MkSystemTime s ns <- getSystemTime
+            let
+                musTime :: Integer
+                musTime = (toInteger s) * 1000000 + div (toInteger ns) 1000
+                musRemaining :: Integer
+                musRemaining = musInterval - (mod musTime musInterval)
+                musNext :: Integer
+                musNext = musTime + musRemaining
+                sysTimeNext :: SystemTime
+                sysTimeNext =
+                    MkSystemTime (fromInteger $ div musNext 1000000) $ fromInteger $ (mod musNext 1000000) * 1000
+                utcTimeNext :: UTCTime
+                utcTimeNext = systemToUTCTime sysTimeNext
+            putMVar var ()
+            threadDelay $ fromInteger musRemaining
+            takeMVar var
+            call utcTimeNext
+    lifeCycleClose $ do
+        takeMVar var
+        throwTo thread MkCancelled
