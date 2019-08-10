3e18de9a68f6e17032fffd13d302df08b5bff814
Ashley Yakeley <ashley@semantic.org>
2019-08-09 22:40:47 -0700

    WIP

--- pinafore/lib/Pinafore/Main.hs
+++ pinafore/lib/Pinafore/Main.hs
@@ -28,24 +28,25 @@ type FilePinaforeType = PinaforeAction PinaforeEdit ()
 filePinaforeType :: Text
 filePinaforeType = qTypeDescription @PinaforeEdit @FilePinaforeType
 
-standardPinaforeSubscriber :: Bool -> FilePath -> LifeCycleIO (Subscriber PinaforeEdit)
-standardPinaforeSubscriber async dirpath = do
+standardPinaforeUpdatingObject :: FilePath -> LifeCycleIO (UpdatingObject PinaforeEdit ())
+standardPinaforeUpdatingObject dirpath = do
     tableObject1 <- lifeCycleWith $ exclusiveObject $ sqlitePinaforeTableObject $ dirpath </> "tables.sqlite3"
     tableObject <- cacheObject 500000 tableObject1 -- half-second delay before writing
     memoryObject <- liftIO makeMemoryCellObject
-    subTable <- makeObjectSubscriber async $ pinaforeTableEntityObject tableObject
-    subFile <- makeObjectSubscriber async $ directoryPinaforeFileObject $ dirpath </> "files"
-    subMemory <- makeObjectSubscriber False memoryObject
-    (subClock, ()) <-
-        makeSharedSubscriber False $
-        clockUpdatingObject (UTCTime (fromGregorian 2000 1 1) 0) (secondsToNominalDiffTime 1)
     let
-        picker :: forall edit. PinaforeSelector edit -> Subscriber edit
-        picker PinaforeSelectPoint = subTable
-        picker PinaforeSelectFile = subFile
-        picker PinaforeSelectMemory = subMemory
-        picker PinaforeSelectClock = subClock
-    return $ tupleSubscribers picker
+        picker :: forall edit. PinaforeSelector edit -> UpdatingObject edit ()
+        picker PinaforeSelectPoint = updatingObject $ pinaforeTableEntityObject tableObject
+        picker PinaforeSelectFile = updatingObject $ directoryPinaforeFileObject $ dirpath </> "files"
+        picker PinaforeSelectMemory = updatingObject memoryObject
+        picker PinaforeSelectClock =
+            clockUpdatingObject (UTCTime (fromGregorian 2000 1 1) 0) (secondsToNominalDiffTime 1)
+    return $ tupleUpdatingObject picker
+
+standardPinaforeSubscriber :: Bool -> FilePath -> LifeCycleIO (Subscriber PinaforeEdit)
+standardPinaforeSubscriber async dirpath = do
+    uobj <- standardPinaforeUpdatingObject dirpath
+    (sub, ()) <- makeSharedSubscriber async uobj
+    return sub
 
 standardPinaforeContext :: Bool -> FilePath -> UIToolkit -> LifeCycleIO (PinaforeContext PinaforeEdit)
 standardPinaforeContext async dirpath toolkit = do
