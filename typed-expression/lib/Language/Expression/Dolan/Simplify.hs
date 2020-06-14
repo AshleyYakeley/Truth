f9c3a8755efeadac5de09124f51f1c06c376c3ef
Ashley Yakeley <ashley@semantic.org>
2020-06-13 17:25:19 -0700

    typed-expression: Simplify: better explanation

--- typed-expression/lib/Language/Expression/Dolan/Simplify.hs
+++ typed-expression/lib/Language/Expression/Dolan/Simplify.hs
@@ -16,18 +16,28 @@ simplifyTypes :: Bool
 simplifyTypes = True
 
 -- Simplification:
--- 1. merge duplicate ground types in join/meet (on each type)
+--
+-- Some simplification happens by the way types are represented.
+-- e.g. "(rec a. P) | Q" => "rec a. (P|Q)"
+-- e.g. "rec a. rec b. (a,b)" => "rec a. (a,a)"
+-- e.g. "(rec a. F a) | (rec a. G a)" => "rec a. (F a|G a)"
+--
+-- eliminateUnusedRecursion: remove unused recursion & eliminate immediate recursion
+-- e.g. "rec a. Integer" => "Integer"
+-- e.g. "rec a. a" => "Any"/"None"
+--
+-- mergeDuplicateGroundTypes: merge duplicate ground types in join/meet (on each type)
 -- e.g. "[a]|[b]" => "[a|b]"
 --
--- 2. eliminate one-sided type vars (on whole expression)
+-- eliminateOneSidedTypeVars: eliminate one-sided type vars (on whole expression)
 -- Type vars are one-sided if they appear only in positive position, or only in negative position.
 -- e.g. "a -> [b]" => "Any -> [None]"
 --
--- 3. merge shared type vars (on whole expression)
+-- mergeSharedTypeVars: merge shared type vars (on whole expression)
 -- Two type variables are shared, if they always appear together (join/meet) in the positive positions, or in the negative positions.
 -- e.g. "a -> b -> a|b" => "a -> a -> a|a"
 --
--- 4. merge duplicate type vars in join/meet (on each type)
+-- mergeDuplicateTypeVars: merge duplicate type vars in join/meet (on each type)
 -- e.g. "a|a" => "a"
 dolanSimplifyTypes ::
        forall (ground :: GroundTypeKind) a.
