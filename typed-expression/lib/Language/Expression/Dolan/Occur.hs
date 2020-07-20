35696e36e48ecb49abaf9d7821a9bed6f8b2b88f
Ashley Yakeley <ashley@semantic.org>
2020-07-19 20:07:50 -0700

    typed-expression: rm unused

--- typed-expression/lib/Language/Expression/Dolan/Occur.hs
+++ typed-expression/lib/Language/Expression/Dolan/Occur.hs
@@ -10,39 +10,6 @@ import Language.Expression.Dolan.TypeSystem
 import Language.Expression.Dolan.Variance
 import Shapes
 
-{-
-class TypeFreeVariables (t :: Type) where
-    typeFreeVariables :: t -> FiniteSet (AnyW SymbolType)
-
-instance forall (ground :: GroundTypeKind) polarity t. IsDolanGroundType ground =>
-             TypeFreeVariables (DolanType ground polarity t) where
-    typeFreeVariables NilDolanType = mempty
-    typeFreeVariables (ConsDolanType t1 tr) = union (typeFreeVariables t1) (typeFreeVariables tr)
-
-instance forall (ground :: GroundTypeKind) polarity t. IsDolanGroundType ground =>
-             TypeFreeVariables (DolanSingularType ground polarity t) where
-    typeFreeVariables (GroundDolanSingularType gt args) = argumentsFreeVariables (groundTypeVarianceType gt) args
-    typeFreeVariables (VarDolanSingularType v) = singletonSet $ MkAnyW v
-    typeFreeVariables (RecursiveDolanSingularType v t) = deleteSet (MkAnyW v) $ typeFreeVariables t
-
-argumentFreeVariables ::
-       forall (ground :: GroundTypeKind) polarity v t. IsDolanGroundType ground
-    => VarianceType v
-    -> SingleArgument v (DolanType ground) polarity t
-    -> FiniteSet (AnyW SymbolType)
-argumentFreeVariables CovarianceType t = typeFreeVariables t
-argumentFreeVariables ContravarianceType t = typeFreeVariables t
-argumentFreeVariables RangevarianceType (MkRangeType p q) = union (typeFreeVariables p) (typeFreeVariables q)
-
-argumentsFreeVariables ::
-       forall (ground :: GroundTypeKind) polarity dv t ta. IsDolanGroundType ground
-    => DolanVarianceType dv
-    -> DolanArguments dv (DolanType ground) t polarity ta
-    -> FiniteSet (AnyW SymbolType)
-argumentsFreeVariables NilListType NilDolanArguments = mempty
-argumentsFreeVariables (ConsListType sv dv) (ConsDolanArguments t1 tr) =
-    union (argumentFreeVariables @ground @polarity sv t1) (argumentsFreeVariables dv tr)
--}
 occursInArg ::
        forall (ground :: GroundTypeKind) polarity n sv a. IsDolanGroundType ground
     => VarianceType sv
