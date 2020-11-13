6143cf469646eb8a719c7c45ebc23daaeb18a7c3
Ashley Yakeley <ashley@semantic.org>
2020-11-12 22:18:55 -0800

    support: dynamic type keywords (#27)

--- support/pygments-lexer/pinafore_lexer/__init__.py
+++ support/pygments-lexer/pinafore_lexer/__init__.py
@@ -21,10 +21,11 @@ class PinaforeLexer(RegexLexer):
             (r'{\#', Comment.Multiline, 'comment'),
             (r'"(\\\\|\\"|[^"])*"', String),
             (words(('rec', 'if', 'then', 'else', 'let', 'in', 'do', 'case', 'of', 'end'), suffix=r'\b'), Keyword),
-            (words(('datatype', 'opentype', 'subtype', 'closedtype'), suffix=r'\b'), Keyword.Declaration),
+            (words(('datatype', 'opentype', 'subtype', 'closedtype', 'dynamictype'), suffix=r'\b'), Keyword.Declaration),
             (words(('property', 'openEntity', 'newOpenEntity', 'evaluate'), suffix=r'\b'), Keyword.Pseudo),
             (words((
-                'Any', 'None', 'Literal', 'Text', 'Number', 'Rational', 'Integer', 'Boolean', 'Ordering', 'Time', 'Duration', 'Date', 'TimeOfDay', 'LocalTime', 'Entity',
+                'Any', 'None', 'Literal', 'Text', 'Number', 'Rational', 'Integer', 'Boolean', 'Ordering', 'Time', 'Duration', 'Date', 'TimeOfDay', 'LocalTime',
+                'Entity', 'DynamicEntity',
                 'Maybe', 'Either', 'RefOrder', 'Action', 'WholeRef', 'SetRef', 'FiniteSetRef', 'UI', 'Window', 'MenuItem'
                 ), suffix=r'\b'), Keyword.Type),
             (r'![-0-9A-Fa-f]+', Literal.Anchor),
