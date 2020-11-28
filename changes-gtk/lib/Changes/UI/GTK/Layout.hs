91304de7d63941b235abdfa4bf9745e3f26a1a80
Ashley Yakeley <ashley@semantic.org>
2020-11-27 20:03:59 -0800

    debug: Layout

--- changes-gtk/lib/Changes/UI/GTK/Layout.hs
+++ changes-gtk/lib/Changes/UI/GTK/Layout.hs
@@ -7,9 +7,10 @@ import Changes.Core
 import Changes.UI.GTK.Useful
 import GI.Gtk
 import Shapes
+import Changes.Debug.Reference
 
 createLayout :: Orientation -> [(Bool, Widget)] -> CreateView Widget
-createLayout orientation contents = do
+createLayout orientation contents = traceBracket "GTK.Layout:create" $ do
     box <- cvNew Box [#orientation := orientation]
     for_ contents $ \(grow, widget) -> #packStart box widget grow grow 0
     toWidget box
