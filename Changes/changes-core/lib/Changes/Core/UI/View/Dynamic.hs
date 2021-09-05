2b3777943972396ca9c48568cb3a0e5d504e5695
Ashley Yakeley <ashley@semantic.org>
2021-09-05 01:33:03 -0700

    changes-core: Dynamic: generalise

--- Changes/changes-core/lib/Changes/Core/UI/View/Dynamic.hs
+++ Changes/changes-core/lib/Changes/Core/UI/View/Dynamic.hs
@@ -1,7 +1,17 @@
-module Changes.Core.UI.View.Dynamic where
+module Changes.Core.UI.View.Dynamic
+    ( DynamicViewState(..)
+    , closeDynamicView
+    , replaceDynamicView
+    , cvDynamic
+    , cvSwitch
+    , cvOneWholeView
+    ) where
 
+import Changes.Core.Edit
 import Changes.Core.Import
 import Changes.Core.Model
+import Changes.Core.Types
+import Changes.Core.UI.Selection
 import Changes.Core.UI.View.CreateView
 import Changes.Core.UI.View.View
 
@@ -24,7 +34,7 @@ replaceDynamicView getNewDVS = do
 cvDynamic ::
        forall dvs update a. (DynamicViewState dvs)
     => Model update
-    -> (Model update -> CreateView (dvs, a))
+    -> CreateView (dvs, a)
     -> Task ()
     -> (a -> [update] -> StateT dvs View ())
     -> CreateView a
@@ -32,7 +42,7 @@ cvDynamic model initCV taskCV recvCV = do
     let
         initBind :: CreateView (MVar dvs, a)
         initBind = do
-            (firstdvs, a) <- initCV model
+            (firstdvs, a) <- initCV
             stateVar <- liftIO $ newMVar firstdvs
             lifeCycleClose $ do
                 lastdvs <- takeMVar stateVar
@@ -43,3 +53,59 @@ cvDynamic model initCV taskCV recvCV = do
     (stateVar, a) <- cvBindModel model Nothing initBind taskCV recvBind
     liftToLifeCycle $ mVarRun stateVar $ recvCV a []
     return a
+
+cvSwitch :: Model (ROWUpdate (CreateView ())) -> CreateView ()
+cvSwitch model = do
+    let
+        getViewState :: CreateView () -> View ViewState
+        getViewState cv = do
+            ((), vs) <- getInnerLifeState cv
+            return vs
+        initVS :: CreateView (ViewState, ())
+        initVS = do
+            firstspec <- viewRunResource model $ \am -> aModelRead am ReadWhole
+            vs <- liftToLifeCycle $ getViewState firstspec
+            return (vs, ())
+        recvVS :: () -> [ROWUpdate (CreateView ())] -> StateT ViewState View ()
+        recvVS () updates = for_ (lastReadOnlyWholeUpdate updates) $ \spec -> replaceDynamicView $ getViewState spec
+    cvDynamic model initVS mempty recvVS
+
+data OneWholeViews f =
+    MkOneWholeViews (f ())
+                    ViewState
+
+instance DynamicViewState (OneWholeViews f) where
+    dynamicViewStates (MkOneWholeViews _ vs) = [vs]
+
+cvOneWholeView ::
+       forall f update. (MonadOne f, IsUpdate update, FullEdit (UpdateEdit update))
+    => Model (FullResultOneUpdate f update)
+    -> (f (Model update) -> CreateView ())
+    -> SelectNotify (f ())
+    -> CreateView ()
+cvOneWholeView model baseView (MkSelectNotify notifyChange) = do
+    let
+        readHasOne ::
+               forall m. MonadIO m
+            => ReaderT ViewContext m (f ())
+        readHasOne = viewRunResource model $ \asub -> aModelRead asub ReadHasOne
+        getWidgets :: f () -> View (OneWholeViews f)
+        getWidgets fu = do
+            notifyChange $ return $ Just fu
+            ((), vs) <-
+                getInnerLifeState $ baseView $ fmap (\() -> mapModel (mustExistOneChangeLens "reference") model) fu
+            return $ MkOneWholeViews fu vs
+        initVS :: CreateView (OneWholeViews f, ())
+        initVS = do
+            firstfu <- readHasOne
+            vs <- liftToLifeCycle $ getWidgets firstfu
+            return (vs, ())
+        recvVS :: () -> [FullResultOneUpdate f update] -> StateT (OneWholeViews f) View ()
+        recvVS () _ = do
+            MkOneWholeViews oldfu vs <- get
+            newfu <- lift readHasOne
+            case (retrieveOne oldfu, retrieveOne newfu) of
+                (SuccessResult (), SuccessResult ()) -> return ()
+                (FailureResult _, FailureResult _) -> put $ MkOneWholeViews newfu vs
+                _ -> replaceDynamicView $ getWidgets newfu
+    cvDynamic model initVS mempty recvVS
--- Changes/changes-gtk/lib/Changes/UI/GTK/Switch.hs
+++ Changes/changes-gtk/lib/Changes/UI/GTK/Switch.hs
@@ -8,23 +8,13 @@ import GI.Gtk hiding (get)
 import Shapes
 
 createDynamic :: Model (ROWUpdate (CreateView Widget)) -> CreateView Widget
-createDynamic sub = do
+createDynamic model = do
     box <- liftIO $ boxNew OrientationVertical 0
     let
-        getViewState :: CreateView Widget -> View ViewState
-        getViewState gview = do
-            ((), vs) <-
-                getInnerLifeState $ do
-                    widget <- gview
-                    cvPackStart True box widget
-                    #showAll widget
-            return vs
-        initVS :: Model (ROWUpdate (CreateView Widget)) -> CreateView (ViewState, ())
-        initVS rm = do
-            firstspec <- viewRunResource rm $ \am -> aModelRead am ReadWhole
-            vs <- liftToLifeCycle $ getViewState firstspec
-            return (vs, ())
-        recvVS :: () -> [ROWUpdate (CreateView Widget)] -> StateT ViewState View ()
-        recvVS () updates = for_ (lastReadOnlyWholeUpdate updates) $ \spec -> replaceDynamicView $ getViewState spec
-    cvDynamic @(ViewState) sub initVS mempty recvVS
+        addWidget :: CreateView Widget -> CreateView ()
+        addWidget cvw = do
+            widget <- cvw
+            cvPackStart True box widget
+            #showAll widget
+    cvSwitch $ mapModel (funcChangeLens addWidget) model
     toWidget box
