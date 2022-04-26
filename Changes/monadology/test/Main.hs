ba1e40af62526bfbfbf194b8284269f39fec90b4
Ashley Yakeley <ashley@semantic.org>
2022-04-26 01:31:37 -0700

    monadology: tests

--- Changes/monadology/test/Main.hs
+++ Changes/monadology/test/Main.hs
@@ -94,8 +94,55 @@ testException =
           pure $ testABracket @(ComposeInner Maybe IO) runComposeInner $ liftInner Nothing
         ]
 
+compareTest :: String -> ((String -> IO ()) -> IO r) -> IO r
+compareTest expected action = do
+    resultsRef <- newIORef ""
+    let
+        appendStr :: String -> IO ()
+        appendStr s = modifyIORef resultsRef $ \t -> t ++ s
+    r <- action appendStr
+    found <- readIORef resultsRef
+    assertEqual "" expected found
+    return r
+
+withMessage :: (String -> IO ()) -> String -> IO r -> IO r
+withMessage appendStr s m = do
+    appendStr $ "+" <> s
+    r <- m
+    appendStr $ "-" <> s
+    return r
+
+testCoroutine :: TestTree
+testCoroutine =
+    testCase "coroutine" $
+    compareTest "+A+B-A-B" $ \appendStr -> do
+        _ <-
+            coroutine
+                (\y ->
+                     withMessage appendStr "A" $ do
+                         y ()
+                         return ((), ()))
+                (\() y ->
+                     withMessage appendStr "B" $ do
+                         y ()
+                         return ((), ()))
+        return ()
+
+testLifeCycle :: TestTree
+testLifeCycle =
+    testCase "lifecycle" $
+    compareTest "ACDB" $ \appendStr -> do
+        let
+            lc :: LifeCycle ()
+            lc = do
+                liftIO $ appendStr "A"
+                lifeCycleOnClose $ appendStr "B"
+                liftIO $ appendStr "C"
+                lifeCycleOnClose $ appendStr "D"
+        runLifeCycleT lc
+
 tests :: TestTree
-tests = testGroup "monadology" [testComposeInner, testException]
+tests = testGroup "monadology" [testCoroutine, testLifeCycle, testComposeInner, testException]
 
 main :: IO ()
 main = defaultMain tests
