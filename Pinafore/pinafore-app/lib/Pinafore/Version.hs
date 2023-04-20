ddb9a962b688a8cf3669965c59c847a69c246617
Ashley Yakeley <ashley@semantic.org>
2023-04-20 01:48:06 -0700

    package-app: gitversion flag

--- Pinafore/pinafore-app/lib/Pinafore/Version.hs
+++ Pinafore/pinafore-app/lib/Pinafore/Version.hs
@@ -1,33 +1,36 @@
+{-# LANGUAGE CPP #-}
 module Pinafore.Version
     ( printVersion
     ) where
 
+#ifdef FLAG_GITVERSION
 import Data.Time
 import GitHash
+#endif
 import Shapes
 
 pinaforeVersion :: String
 pinaforeVersion = "0.4"
 
-gi :: GitInfo
-gi = $$tGitInfoCwd
-
-commitZonedTime :: ZonedTime
-commitZonedTime = parseTimeOrError True defaultTimeLocale "%a %b %-e %T %Y %z" (giCommitDate gi)
-
-commitTimeString :: String
-commitTimeString = formatTime defaultTimeLocale "%FT%TZ" $ zonedTimeToUTC commitZonedTime
+extraVersion :: String
+extraVersion =
+#ifdef FLAG_GITVERSION
+    let
+    gi :: GitInfo
+    gi = $$tGitInfoCwd
+    commitZonedTime :: ZonedTime
+    commitZonedTime = parseTimeOrError True defaultTimeLocale "%a %b %-e %T %Y %z" (giCommitDate gi)
+    commitTimeString :: String
+    commitTimeString = formatTime defaultTimeLocale "%FT%TZ" $ zonedTimeToUTC commitZonedTime
+    flag :: String
+    flag =
+        if giDirty gi
+            then "+"
+            else ""
+    in " (" <> commitTimeString <> " " <> giHash gi <> ")" <> flag
+#else
+    ""
+#endif
 
 printVersion :: IO ()
-printVersion =
-    putStrLn $
-    "Pinafore version " <>
-    pinaforeVersion <>
-    " (" <>
-    commitTimeString <>
-    " " <>
-    giHash gi <>
-    ")" <>
-    if giDirty gi
-        then "+"
-        else ""
+printVersion = putStrLn $ "Pinafore version " <> pinaforeVersion <> extraVersion
