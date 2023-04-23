0d4564f6a454e79bba3aed02db4475eb28f5bef6
Ashley Yakeley <ashley@semantic.org>
2023-04-23 01:48:00 -0700

    flake.nix: fix gitversion flag, other improvements

--- flake.nix
+++ flake.nix
@@ -1,6 +1,3 @@
-# nix build .?submodules=1
-# https://input-output-hk.github.io/haskell.nix/
-# source: https://github.com/input-output-hk/haskell.nix
 {
     description = "Pinafore";
     inputs =
@@ -10,43 +7,57 @@
         flake-utils.url = "github:numtide/flake-utils";
     };
     outputs = { self, nixpkgs, flake-utils, haskellNix }:
-        flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
+        let
+        supportedSystems =
+        [
+            "x86_64-linux"
+            "x86_64-darwin"
+            "aarch64-linux"
+            "aarch64-darwin"
+        ];
+        in flake-utils.lib.eachSystem supportedSystems
         (system:
             let
                 overlays = [ haskellNix.overlay
                     (final: prev:
                     {
-                        # This overlay adds our project to pkgs
-                        pinaforeProject =
-                        final.haskell-nix.project'
+                        pinaforeProject = final.haskell-nix.stackProject'
                         {
                             src = ./.;
-                            compiler-nix-name = "ghc925";
-                            # This is used by `nix develop .` to open a shell for use with
-                            # `cabal`, `hlint` and `haskell-language-server`
-                            shell.tools =
+                            evalSystem = "x86_64-linux";
+                            modules = [{packages."pinafore-app".configureFlags = ["-f" "-gitversion"];}];
+
+                            # For `nix develop .?submodules=1`
+                            shell =
                             {
-                                cabal = {};
-                                hlint = {};
-                                haskell-language-server = {};
+                                tools =
+                                {
+                                    cabal = {};
+                                    hlint = {};
+                                    haskell-language-server = {};
+                                };
+                                # Non-Haskell shell tools go here
+                                buildInputs = with pkgs; [ nixpkgs-fmt ];
                             };
-                            # Non-Haskell shell tools go here
-                            shell.buildInputs = with pkgs; [ nixpkgs-fmt ];
-                            # This adds `js-unknown-ghcjs-cabal` to the shell.
-                            # shell.crossPlatforms = p: [p.ghcjs];
                         };
                     })
                     ];
                 pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-                flake = pkgs.pinaforeProject.flake
-                {
-                    # This adds support for `nix build .#js-unknown-ghcjs:pinafore:exe:pinafore`
-                    # crossPlatforms = p: [p.ghcjs];
-                };
+                flake = pkgs.pinaforeProject.flake {};
             in flake //
             {
-                # Built by `nix build .`
+                # Built by `nix build .?submodules=1`
                 packages.default = flake.packages."pinafore-app:exe:pinafore";
             }
         );
+
+    # Flake local Nix configuration
+    nixConfig =
+    {
+        # This sets the flake to use the IOG nix cache.
+        # Nix should ask for permission before using it, but remove it here if you do not want it to.
+        extra-substituters = ["https://cache.iog.io"];
+        extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+        allow-import-from-derivation = "true";
+    };
 }
