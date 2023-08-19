43a4ea4b9fff61e3f756909cb060743c033e326f
Ashley Yakeley <ashley@semantic.org>
2023-08-19 16:39:29 -0700

    lib: add forever, tryStop, tryStop_ (#221)

--- Pinafore/pinafore-language/lib/Pinafore/Language/Library/Action.hs
+++ Pinafore/pinafore-language/lib/Pinafore/Language/Library/Action.hs
@@ -18,6 +18,12 @@ qfail t = fail $ unpack t
 onStop :: Action A -> Action A -> Action A
 onStop p q = p <|> q
 
+tryStop :: Action A -> Action (Maybe A)
+tryStop action = actionLiftView $ fmap knowToMaybe $ unliftAction action
+
+tryStop_ :: Action () -> Action ()
+tryStop_ action = actionLiftView $ fmap (\_ -> ()) $ unliftAction action
+
 actionLibSection :: BindDocStuff context
 actionLibSection =
     headingBDS
@@ -34,6 +40,19 @@ actionLibSection =
                 "Stop. This is similar to an exception that can be caught with `onStop`. The default handler (for the main program, button presses, etc.), is to catch and ignore it."
                 (empty :: Action BottomType)
           , addNameInRootBDS $ valBDS "onStop" "`onStop p q` does `p` first, and if it stops, then does `q`." $ onStop
+          , addNameInRootBDS $
+            valBDS
+                "forever"
+                "Run this action repeatedly forever. Use `stop` to break out, propagating the stop. Same as `fn x => let rec fx = x >> fx in fx`." $
+            (forever :: Action () -> Action BottomType)
+          , addNameInRootBDS $
+            valBDS
+                "tryStop"
+                "Run action. If it stops, catch and return `Nothing`. Same as `fn x => onStop (map.Action Just x) $ pure Nothing`." $
+            tryStop
+          , addNameInRootBDS $
+            valBDS "tryStop_" "Run action. If it stops, catch and return `()`. Same as `fn x => onStop x $ pure ()`." $
+            tryStop_
           , addNameInRootBDS $
             valBDS "for_" "Perform an action on each value of a list." (for_ :: [A] -> (A -> Action ()) -> Action ())
           , addNameInRootBDS $
