a760965433de07e7adfbd1fa5c955663305f13eb
Ashley Yakeley <ashley@semantic.org>
2024-04-12 20:22:28 -0700

    Sphinx (#216): ReadTheDocs theme

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,7 @@ release = '0.5'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser","sphinxcontrib.fulltoc"]
+extensions = ["myst_parser","sphinx_rtd_theme"]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
