533d79929ee359cab18174148821898901d591a0
Ashley Yakeley <ashley@semantic.org>
2024-04-13 22:19:26 -0700

    Sphinx (#216): clean up conf, remove sourcelinks

--- doc/conf.py
+++ doc/conf.py
@@ -5,25 +5,20 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'Pinafore'
-copyright = '2024, Ashley Yakeley'
-author = 'Ashley Yakeley'
-release = '0.5'
+project = "Pinafore"
+copyright = "2024, Ashley Yakeley"
+author = "Ashley Yakeley"
+release = "0.5"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = ["myst_parser","sphinx_rtd_theme"]
-
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "sphinx_rtd_theme"
-html_static_path = ['_static']
+html_static_path = ["_static"]
 html_css_files = ["extra.css"]
+html_show_sourcelink = False
