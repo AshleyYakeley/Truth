b5385e14bfc4b1c09e12c5eee6f4afea297b93fe
Ashley Yakeley <ashley@semantic.org>
2024-06-05 23:56:10 -0700

    Library: MediaType type (#271)

--- Pinafore/pinafore-language/lib/Pinafore/Language/Library/Entity/Media.hs
+++ Pinafore/pinafore-language/lib/Pinafore/Language/Library/Entity/Media.hs
@@ -4,15 +4,24 @@ module Pinafore.Language.Library.Entity.Media
     ( mediaEntityLibSection
     ) where
 
+import Changes.World.Media.Type
 import Import
 import Pinafore.Language.Convert.HasType
 import Pinafore.Language.Convert.Literal
 import Pinafore.Language.Library.Defs
+import Pinafore.Language.Library.Entity.Blob ()
 import Pinafore.Language.Library.Entity.Literal
 import Pinafore.Language.Library.LibraryModule
 import Pinafore.Language.Library.Optics ()
 import Pinafore.Language.Type
 import Pinafore.Language.Value
+
+-- MediaType
+mediaTypeGroundType :: QGroundType '[] MediaType
+mediaTypeGroundType = mkLiteralGroundType $(iowitness [t|'MkWitKind (SingletonFamily MediaType)|]) "Type.Media."
+
+instance HasQGroundType '[] MediaType where
+    qGroundType = mediaTypeGroundType
 
 -- Media
 mediaGroundType :: QGroundType '[] Media
@@ -29,7 +38,22 @@ mediaEntityLibSection =
     headingBDS
         "Media"
         ""
-        [ typeBDS "Media" "" (MkSomeGroundType mediaGroundType) []
+        [ namespaceBDS
+              "Media"
+              [ typeBDS
+                    "Type"
+                    "RFC 6838 media type."
+                    (MkSomeGroundType mediaTypeGroundType)
+                    [ valPatBDS "Mk" "Type, subtype, parameters" MkMediaType $
+                      PureFunction $ \(MkMediaType t s p) -> (t, (s, (p, ())))
+                    ]
+              , literalSubtypeRelationEntry @MediaType
+              ]
+        , typeBDS
+              "Media"
+              "A blob and an RFC 6838 media type that interprets it."
+              (MkSomeGroundType mediaGroundType)
+              [valPatBDS "Mk" "Type and Blob" MkMedia $ PureFunction $ \(MkMedia t b) -> (t, (b, ()))]
         , literalSubtypeRelationEntry @Media
         , namespaceBDS "Media" [valBDS "text" "" textMedia]
         ]
