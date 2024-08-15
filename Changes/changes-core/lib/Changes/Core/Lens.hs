ef45f8bf1fb7c02f454672af059098bb981c61a2
Ashley Yakeley <ashley@semantic.org>
2024-08-15 00:40:33 -0700

    changes-core: delayUpdateLens

--- Changes/changes-core/lib/Changes/Core/Lens.hs
+++ Changes/changes-core/lib/Changes/Core/Lens.hs
@@ -2,6 +2,7 @@ module Changes.Core.Lens
     ( module I
     ) where
 
+import Changes.Core.Lens.Delay as I
 import Changes.Core.Lens.Floating as I
 import Changes.Core.Lens.Lens as I
 import Changes.Core.Lens.State as I
---
+++ Changes/changes-core/lib/Changes/Core/Lens/Delay.hs
@@ -0,0 +1,16 @@
+module Changes.Core.Lens.Delay where
+
+import Changes.Core.Import
+import Changes.Core.Lens.Lens
+
+-- | Mostly for debugging
+delayUpdateLens :: Int -> GenChangeLens lin update update
+delayUpdateLens mus =
+    MkChangeLens
+        { clRead = \rd -> rd
+        , clUpdate =
+              \update _ -> do
+                  liftIO $ threadDelay mus
+                  return [update]
+        , clPutEdits = \edits _ -> return $ Just edits
+        }
