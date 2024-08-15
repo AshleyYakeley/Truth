3460981c2fabfcd5c27458243c44d6e5fc744287
Ashley Yakeley <ashley@semantic.org>
2024-08-15 01:01:51 -0700

    issue #304: sort out test

--- Changes/changes-core/test/List.hs
+++ Changes/changes-core/test/List.hs
@@ -20,7 +20,10 @@ actionModelPush model edits = do
 langListModelItem ::
        forall t. Bool -> Int64 -> WModel (ListUpdate (WholeUpdate t)) -> Lifecycle (WModel (WholeUpdate (Maybe t)))
 langListModelItem present i lmodel =
-    eaFloatMap emptyResourceContext (changeLensToFloating (bijectionWholeChangeLens id) . listItemLens present (MkSequencePoint i)) lmodel
+    eaFloatMap
+        emptyResourceContext
+        (changeLensToFloating (bijectionWholeChangeLens id) . listItemLens present (MkSequencePoint i))
+        lmodel
 
 testIssue304 :: TestTree
 testIssue304 =
@@ -28,30 +31,31 @@ testIssue304 =
     testTree "issue-304" $ do
         np <- getNumProcessors
         refPutRestore processorCountRef np $
-            runLifecycle $
-            for_ [1 .. 5000] $ \(i :: Integer) -> do
-                ref <- liftIO $ makeMemoryReference mempty $ \_ -> True
-                model <- makeReflectingModel $ convertReference ref
-                let
-                    checkRef :: String -> [Int] -> Lifecycle ()
-                    checkRef name expected = do
-                        foundv <- liftIO $ runResource emptyResourceContext ref $ \aref -> refRead aref ReadWhole
-                        let found = toList foundv
-                        if found == expected
-                            then return ()
-                            else fail $ "iter #" <> show i <> " " <> name <> ": " <> show found
-                    lm :: WModel (ListUpdate (WholeUpdate Int))
-                    lm = MkWModel model
-                    wm = eaMap (bijectionWholeChangeLens id) lm
-                checkRef "A" []
-                actionModelPush wm $ pure $ MkWholeReaderEdit $ fromList [10, 20, 30]
-                checkRef "B" [10, 20, 30]
-                -- liftIO $ threadDelay 100
-                im <- langListModelItem False 1 lm
-                checkRef "C" [10, 20, 30]
-                liftIO $ threadDelay 100
-                actionModelPush im $ pure $ MkWholeReaderEdit $ Just 15
-                checkRef "D" [10, 15, 20, 30]
+            for_ [1 .. 5000] $ \(i :: Integer) ->
+                runLifecycle $ do
+                    ref <- liftIO $ makeMemoryReference mempty $ \_ -> True
+                    model <- makeReflectingModel $ convertReference ref
+                    let
+                        checkRef :: String -> [Int] -> Lifecycle ()
+                        checkRef name expected = do
+                            foundv <- liftIO $ runResource emptyResourceContext ref $ \aref -> refRead aref ReadWhole
+                            let found = toList foundv
+                            if found == expected
+                                then return ()
+                                else fail $ "iter #" <> show i <> " " <> name <> ": " <> show found
+                        lm :: WModel (ListUpdate (WholeUpdate Int))
+                        lm = MkWModel model
+                        wm = eaMap (bijectionWholeChangeLens id) lm
+                    checkRef "A" []
+                    actionModelPush wm $ pure $ MkWholeReaderEdit $ fromList [10, 20, 30]
+                    checkRef "B" [10, 20, 30]
+                    -- liftIO $ threadDelay 100 -- stops issue
+                    im <- langListModelItem False 1 lm
+                    checkRef "C" [10, 20, 30]
+                    liftIO $ threadDelay 300
+                    actionModelPush im $ pure $ MkWholeReaderEdit $ Just 15
+                    checkRef "D" [10, 15, 20, 30]
+                    liftIO $ taskWait $ modelCommitTask model
 
 testList :: TestTree
 testList = testTree "list" [testIssue304]
