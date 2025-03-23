b102a54999ca89ffd8fbac3f63d3e0cd6f6de4fa
Ashley Yakeley <ashley@semantic.org>
2025-03-22 17:25:59 -0700

    WebKit URI schemes

--- Changes/changes-gnome/lib/Changes/World/GNOME/GTK/Widget/WebView.hs
+++ Changes/changes-gnome/lib/Changes/World/GNOME/GTK/Widget/WebView.hs
@@ -1,21 +1,50 @@
 module Changes.World.GNOME.GTK.Widget.WebView
-    ( createWebView
+    ( WebViewOptions (..)
+    , defaultWebViewOptions
+    , createWebView
     )
 where
 
 import Changes.Core
+import GI.Gio
 import GI.Gtk
 import GI.WebKit2
+import Network.URI
 import Shapes
 
 import Changes.World.GNOME.GI
 
-createWebView :: Model (ROWUpdate Text) -> GView 'Unlocked Widget
-createWebView lmod = do
+data WebViewOptions = MkWebViewOptions
+    { wvoURISchemes :: [(Text, Text -> URI -> IO (Maybe Media))]
+    }
+
+defaultWebViewOptions :: WebViewOptions
+defaultWebViewOptions =
+    MkWebViewOptions
+        { wvoURISchemes = []
+        }
+
+createWebView :: WebViewOptions -> Model (ROWUpdate Text) -> GView 'Unlocked Widget
+createWebView MkWebViewOptions{..} lmod = do
     (wv, widget) <-
         gvRunLocked $ do
-            wv <- gvNew WebView []
-            widget <- toWidget wv
-            return (wv, widget)
+            webContext <- webContextNewEphemeral
+            gvAcquire webContext
+            for_ wvoURISchemes $ \(name, call) -> webContextRegisterUriScheme webContext name $ \request -> do
+                method <- uRISchemeRequestGetHttpMethod request
+                uriText <- uRISchemeRequestGetUri request
+                case parseURIReference (unpack uriText) of
+                    Just uri -> do
+                        mMedia <- call method uri
+                        for_ mMedia $ \(MkMedia mt bs) -> do
+                            stream <- memoryInputStreamNewFromData bs Nothing
+                            uRISchemeRequestFinish request stream (fromIntegral $ olength bs) $ Just $ encode textMediaTypeCodec mt
+                    Nothing -> do
+                        err <- gerrorNew 0 0 $ "bad URI: " <> uriText
+                        uRISchemeRequestFinishError request err
+            webView <- webViewNewWithContext webContext
+            gvAcquire webView
+            widget <- toWidget webView
+            return (webView, widget)
     gvBindReadOnlyWholeModel lmod $ \text -> gvRunLocked $ webViewLoadHtml wv text Nothing
     return widget
--- Pinafore/pinafore-lib-gnome/lib/Pinafore/Library/WebKit.hs
+++ Pinafore/pinafore-lib-gnome/lib/Pinafore/Library/WebKit.hs
@@ -5,16 +5,38 @@ where
 
 import Changes.Core
 import Changes.World.GNOME.GTK
+import Data.Shim ()
+import Network.URI
 import Pinafore.API
 import Pinafore.Library.Media
 import Shapes
 
 import Pinafore.Library.GTK.Widget
 
-webViewWidget :: ImmutableWholeModel HTMLText -> LangWidget
-webViewWidget model =
-    MkLangWidget $ \_ -> createWebView $ unWModel $ immutableWholeModelValue mempty $ fmap unHTMLText model
+type WebViewOptionsSig :: [Type]
+type WebViewOptionsSig = '[[(Text, Text -> URI -> Action Media)]]
+
+webViewType :: ListType QDocSignature WebViewOptionsSig
+webViewType =
+    ConsListType
+        (mkValueDocSignature "uriSchemes" "" $ Just [])
+        NilListType
+
+wvOptions :: (View --> IO) -> ListProduct WebViewOptionsSig -> WebViewOptions
+wvOptions unliftIO (uriSchemes, ()) =
+    defaultWebViewOptions
+        { wvoURISchemes = (fmap $ fmap $ fmap $ fmap $ fmap knowToMaybe . unliftIO . unliftAction) uriSchemes
+        }
+
+webViewVal :: ListProduct WebViewOptionsSig -> ImmutableWholeModel HTMLText -> LangWidget
+webViewVal opts model =
+    MkLangWidget $ \wc -> createWebView (wvOptions (wcUnlift wc) opts) $ unWModel $ immutableWholeModelValue mempty $ fmap unHTMLText model
 
 webKitStuff :: LibraryStuff
 webKitStuff =
-    headingBDS "WebKit" "WebKit HTML rendering, etc." $ pure $ namespaceBDS "WebKit" [valBDS "webView" "" webViewWidget]
+    headingBDS "WebKit" "WebKit HTML rendering, etc."
+        $ pure
+        $ namespaceBDS
+            "WebKit"
+            [ recordValueBDS "webView" "" webViewType webViewVal
+            ]
