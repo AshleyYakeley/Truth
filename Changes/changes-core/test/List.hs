fa9a51ea0778a70502517c6e759b36e6d9c43787
Ashley Yakeley <ashley@semantic.org>
2026-07-11 11:05:26 -0700

    Tests (ignored) for #352

--- Changes/changes-core/test/List.hs
+++ Changes/changes-core/test/List.hs
@@ -62,5 +62,40 @@ testIssue304 =
                         checkRef "D" [10, 15, 20, 30]
                         liftIO $ taskWait $ modelCommitsTask model
 
+testIssue352 :: TestTree
+testIssue352 = ignoreTestBecause "ISSUE 352"
+    $ localOption (mkTimeout 20000000)
+    $ testTree "issue-352"
+    $ do
+        np <- getNumProcessors
+        refPutRestore processorCountRef np
+            $ for_ [1 .. 500]
+            $ \(i :: Integer) ->
+                runLifecycle $ do
+                    ref <- liftIO $ makeMemoryReference mempty $ \_ -> True
+                    model <- makeReflectingModel $ convertReference ref
+                    let
+                        checkRef :: String -> [Int] -> Lifecycle ()
+                        checkRef name expected = do
+                            foundv <- liftIO $ runResource emptyResourceContext ref $ \aref -> refRead aref ReadWhole
+                            let found = toList foundv
+                            if found == expected
+                                then return ()
+                                else fail $ "iter #" <> show i <> " " <> name <> ": " <> show found
+                        lm :: WModel (ListUpdate (WholeUpdate Int))
+                        lm = MkWModel model
+                        wm = eaMap (bijectionWholeChangeLens id) lm
+                    checkRef "A" []
+                    actionModelPush wm $ pure $ MkWholeReaderEdit $ fromList [10, 20, 30]
+                    checkRef "B" [10, 20, 30]
+                    im <- langListModelItem True 1 lm
+                    checkRef "C" [10, 20, 30]
+                    actionModelPush lm $ pure $ ListEditInsert 1 12
+                    checkRef "D" [10, 12, 20, 30]
+                    -- liftIO $ threadDelay 300 -- stops issue
+                    actionModelPush im $ pure $ MkWholeReaderEdit $ Just 15
+                    checkRef "E" [10, 12, 15, 30]
+                    liftIO $ taskWait $ modelCommitsTask model
+
 testList :: TestTree
-testList = testTree "list" [testIssue304]
+testList = testTree "list" [testIssue304, testIssue352]
